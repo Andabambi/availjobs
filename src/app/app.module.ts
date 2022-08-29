cf8da592761bad9d09cb7dead6fbd0ba4406ffa2
Andabambi <104838926+Andabambi@users.noreply.github.com>
2022-08-29 23:16:37 +0200

    availjobs

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,16 +3,24 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LandingComponent,
+    SignupComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
+import { MatSliderModule } from '@angular/material/slider';
+import { LandingComponent } from './landing/landing.component';
+import { SignupComponent } from './Components/signup/signup.component';
+
